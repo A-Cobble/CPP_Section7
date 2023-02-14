a5ac18042a062777d956b44ec1c1f6af5325c718
A-Cobble <alex.cobble42@gmail.com>
2023-02-14 12:24:03 -0800

    Practiced initiallizing arrays, and changing the elements inside an array using user input.

--- main.cpp
+++ main.cpp
@@ -26,5 +26,43 @@ int main() {
 
 	int anotherArray[] { 1,2,3,4,5 }; // size automatically calculated
 
+
+
+	char vowels[]{ 'a', 'e', 'i', 'o', 'u' };
+	cout << "\nThe first vowel is: " << vowels[0] << endl;
+	cout << "The last vowel is: " << vowels[4] << endl;
+
+	//cin >> vowels[5]; the causes an error because it is out of bounds
+
+	double hiTemps[]{ 90.1, 89.8, 77.5, 81.6 };
+	cout << "\nThe first high temperature is: " << hiTemps[0] << endl;
+
+	hiTemps[0] = 100.7; //set the first element in hiTemps to 100.7
+	cout << "The first high temperature is now: " << hiTemps[0] << endl;
+
+	int scores[] {100, 90, 80, 70, 60,};
+
+	//accessing array elements with user input
+	cout << "\nFirst score at index 0: " << scores[0] << endl;
+	cout << "Second score at index 1: " << scores[1] << endl;
+	cout << "Third score at index 2: " << scores[2] << endl;
+	cout << "Fourth score at index 3: " << scores[3] << endl;
+	cout << "Fifth score at index 4: " << scores[4] << endl;
+	cout << "\nEnter 5 test scores: ";
+	cin >> scores[0];
+	cin >> scores[1];
+	cin >> scores[2];
+	cin >> scores[3];
+	cin >> scores[4];
+	cout << "\nThe updated array is: " << endl;
+	cout << "First score at index 0: " << scores[0] << endl;
+	cout << "Second score at index 1: " << scores[1] << endl;
+	cout << "Third score at index 2: " << scores[2] << endl;
+	cout << "Fourth score at index 3: " << scores[3] << endl;
+	cout << "Fifth score at index 4: " << scores[4] << endl;
+
+	cout << "\nNotice what the value of the array name is: " << scores << endl;
+	cout << endl;
+
 	return 0;
 }
